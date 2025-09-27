1b9a0586ece5fd7147c168d8099e0b9f4c108241
JessQiu <JessQiu@outlook.com>
2025-09-27 17:08:18 +0800

    feat(host): 重构程序入口并添加Serilog日志配置
    重构Program.cs文件，添加启动标语和异常处理逻辑
    更新appsettings.json配置文件，使用Serilog替代默认日志系统

--- src/TinyAbp.HttpApi.Host/Program.cs
+++ src/TinyAbp.HttpApi.Host/Program.cs
@@ -1,8 +1,46 @@
-// 主程序入口点
-using TinyAbp.Framework.AspNetCore.Microsoft.AspNetCore.Builder;
+using Serilog;
+using TinyAbp.HttpApi.Host;
 
-var builder = WebApplication.CreateBuilder(args).RegisterAutofacContainer().RegisterListenAddress();
+Console.WriteLine(
+    """
+     _____ ___ _  ___   __    _   ___ ___   ___                                  _   
+    |_   _|_ _| \| \ \ / /   /_\ | _ ) _ \ | __| _ __ _ _ __  _____ __ _____ _ _| |__
+      | |  | || .` |\ V /   / _ \| _ \  _/ | _| '_/ _` | '  \/ -_) V  V / _ \ '_| / /
+      |_| |___|_|\_| |_|   /_/ \_\___/_|   |_||_| \__,_|_|_|_\___|\_/\_/\___/_| |_\_\
+                                                                                     
+    """
+);
 
-var app = builder.Build();
+var builder = WebApplication.CreateBuilder(args)
+    .RegisterSerilogLogger()
+    .RegisterAutofacContainer()
+    .RegisterListenAddress();
 
-app.Run();
+// 记录当前主机启动环境
+Log.Information($"当前主机启动环境 - {builder.Environment.EnvironmentName}");
+
+
+try
+{
+    // 添加应用程序模块
+    await builder.Services.AddApplicationAsync<TinyAbpHttpApiHostModule>();
+
+    // 构建Web应用程序
+    var app = builder.Build();
+
+    // 初始化应用程序
+    await app.InitializeApplicationAsync();
+
+    // 运行应用程序
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    // 记录致命错误
+    Log.Fatal(ex.Message);
+}
+finally
+{
+    // 关闭并刷新日志
+    await Log.CloseAndFlushAsync();
+}
