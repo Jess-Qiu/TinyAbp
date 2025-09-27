1ba23bf66376db6e1efc85f27b9fac8fc4a75f67
JessQiu <JessQiu@outlook.com>
2025-09-27 16:42:43 +0800

    feat(日志): 添加Serilog日志模块及依赖
    添加TinyAbpFrameworkLoggerSerilogModule模块类并配置项目文件
    引入Serilog.AspNetCore和Serilog.Sinks.Async包依赖
    添加对TinyAbp.Framework.Core的项目引用

---
+++ framework/TinyAbp.Framework.Logger.Serilog/TinyAbpFrameworkLoggerSerilogModule.cs
@@ -0,0 +1,8 @@
+using Volo.Abp.Modularity;
+
+namespace TinyAbp.Framework.Logger.Serilog;
+
+/// <summary>
+/// Tiny Abp Framework Logger Serilog Module - Serilog日志模块
+/// </summary>
+public class TinyAbpFrameworkLoggerSerilogModule : AbpModule { }
