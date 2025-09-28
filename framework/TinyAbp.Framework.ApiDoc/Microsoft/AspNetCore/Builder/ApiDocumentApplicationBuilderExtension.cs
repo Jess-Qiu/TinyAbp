eb12cb78f859b1554fa14fa871e3025249083a2c
JessQiu <JessQiu@outlook.com>
2025-09-28 16:11:27 +0800

    refactor(ApiDoc): 重构API文档模块为扩展方法
    将原有的模块化实现改为扩展方法，提供更灵活的API文档配置
    移除TinyAbpFrameworkApiDocModule，新增ApiDocumentApplicationBuilderExtension和ApiDocumentServiceCollectionExtension
    简化项目文件结构，移除多余空行

---
+++ framework/TinyAbp.Framework.ApiDoc/Microsoft/AspNetCore/Builder/ApiDocumentApplicationBuilderExtension.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Scalar.AspNetCore;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace Microsoft.AspNetCore.Builder;
+
+public static class ApiDocumentApplicationBuilderExtension
+{
+    public static IApplicationBuilder UseApiDocument(
+        this IApplicationBuilder app,
+        string defaultApiDocumentName = "default"
+    )
+    {
+        if (app == null)
+        {
+            throw new ArgumentNullException(nameof(app));
+        }
+
+        var swaggerGenOptions = app
+            .ApplicationServices.GetRequiredService<IOptions<SwaggerGenOptions>>()
+            .Value;
+
+        app.UseEndpoints(config =>
+        {
+            config.MapSwagger("/openapi/{documentName}.json");
+            config.MapScalarApiReference(options => options.AddDocument(defaultApiDocumentName));
+        });
+
+        return app;
+    }
+}
---
+++ framework/TinyAbp.Framework.ApiDoc/Microsoft/Extensions/DependencyInjection/ApiDocumentServiceCollectionExtension.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.Conventions;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ApiDocumentServiceCollectionExtension
+{
+    public static IServiceCollection AddApiDocument(
+        this IServiceCollection services,
+        Action<SwaggerGenOptions>? action = null
+    )
+    {
+        var mvcOptions = services.GetPreConfigureActions<AbpAspNetCoreMvcOptions>().Configure();
+
+        var remoteServiceSettings =
+            mvcOptions.ConventionalControllers.ConventionalControllerSettings.DistinctBy(x =>
+                x.RemoteServiceName
+            );
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            if (action == null)
+            {
+                options.SwaggerDoc(
+                    "default",
+                    new OpenApiInfo { Title = "TinyAbp Framework API Document", Version = "v1" }
+                );
+            }
+            else
+            {
+                action?.Invoke(options);
+            }
+
+            ConfigurateApiDocGroups(options, remoteServiceSettings);
+            ConfigurateApiDocFilter(options, remoteServiceSettings);
+        });
+
+        return services;
+    }
+
+    private static void ConfigurateApiDocGroups(
+        SwaggerGenOptions options,
+        IEnumerable<ConventionalControllerSetting> remoteServiceSettings
+    )
+    {
+        if (remoteServiceSettings.Any())
+        {
+            foreach (var setting in remoteServiceSettings)
+            {
+                options.SwaggerDoc(
+                    setting.RemoteServiceName,
+                    new() { Title = setting.RemoteServiceName, Version = "v1" }
+                );
+            }
+        }
+    }
+
+    private static void ConfigurateApiDocFilter(
+        SwaggerGenOptions options,
+        IEnumerable<ConventionalControllerSetting> remoteServiceSettings
+    )
+    {
+        if (remoteServiceSettings.Any())
+        {
+            options.DocInclusionPredicate(
+                (docName, apiDesc) =>
+                {
+                    if (
+                        apiDesc.ActionDescriptor
+                        is ControllerActionDescriptor controllerActionDescriptor
+                    )
+                    {
+                        var matchingSetting = remoteServiceSettings.FirstOrDefault(setting =>
+                            setting.Assembly
+                            == controllerActionDescriptor.ControllerTypeInfo.Assembly
+                        );
+
+                        return matchingSetting?.RemoteServiceName == docName;
+                    }
+
+                    return false;
+                }
+            );
+        }
+    }
+}
