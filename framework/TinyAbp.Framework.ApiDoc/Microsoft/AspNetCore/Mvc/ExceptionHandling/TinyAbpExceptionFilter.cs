60c867ba62391a1ca527c12122df5258dc51bddc
JessQiu <JessQiu@outlook.com>
2025-09-28 17:13:09 +0800

    feat(异常处理): 添加TinyAbpExceptionFilter处理异常响应格式
    自定义异常过滤器，继承AbpExceptionFilter并重写HandleAndWrapException方法，将RemoteServiceErrorResponse的Error属性作为最终响应内容返回，以统一异常响应格式

---
+++ framework/TinyAbp.Framework.ApiDoc/Microsoft/AspNetCore/Mvc/ExceptionHandling/TinyAbpExceptionFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Volo.Abp.AspNetCore.Mvc.ExceptionHandling;
+using Volo.Abp.Http;
+
+namespace Microsoft.AspNetCore.Mvc.ExceptionHandling;
+
+public class TinyAbpExceptionFilter : AbpExceptionFilter
+{
+    protected override async Task HandleAndWrapException(ExceptionContext context)
+    {
+        await base.HandleAndWrapException(context);
+
+        if (context.Result is ObjectResult objectResult)
+        {
+            if (objectResult.Value is RemoteServiceErrorResponse errorInfoResult)
+            {
+                context.Result = new ObjectResult(errorInfoResult.Error);
+            }
+        }
+    }
+}
