babdcea58412670cd2d1eccea762728dcd64c9e9
JessQiu <JessQiu@outlook.com>
2025-09-30 15:00:41 +0800

    feat(ddd): 添加Abp.FluentValidation依赖以支持验证功能
    在DDD应用层添加Volo.Abp.FluentValidation包依赖，用于实现模型验证功能

--- framework/TinyAbp.Framework.Ddd.Application/TinyAbpFrameworkDddApplicationModule.cs
+++ framework/TinyAbp.Framework.Ddd.Application/TinyAbpFrameworkDddApplicationModule.cs
@@ -1,9 +1,14 @@
 using Volo.Abp.Application;
+using Volo.Abp.FluentValidation;
 using Volo.Abp.Modularity;
 
 /// <summary>
 /// TinyAbp框架DDD应用程序模块
 /// 负责配置和注册DDD应用程序层相关服务
 /// </summary>
-[DependsOn(typeof(TinyAbpFrameworkDddApplicationContractsModule), typeof(AbpDddApplicationModule))]
+[DependsOn(
+    typeof(TinyAbpFrameworkDddApplicationContractsModule),
+    typeof(AbpDddApplicationModule),
+    typeof(AbpFluentValidationModule)
+)]
 public class TinyAbpFrameworkDddApplicationModule : AbpModule { }
