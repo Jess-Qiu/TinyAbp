eb57b38a877ad2523a133377bf86004781045e49
JessQiu <JessQiu@outlook.com>
2025-10-01 02:06:09 +0800

    feat(redis): 添加FreeRedis分布式锁和缓存模块
    添加基于FreeRedis的分布式锁和缓存模块实现
    更新项目配置和依赖项以支持新功能
    完善API文档模块的描述和标签信息

---
+++ framework/TinyAbp.Framework.DistributedLock.FreeRedis/TinyAbpFrameworkDistributedLockFreeRedisModule.cs
@@ -0,0 +1,9 @@
+﻿using Volo.Abp.Modularity;
+
+namespace TinyAbp.Framework.DistributedLock.FreeRedis;
+
+/// <summary>
+/// TinyAbp FreeRedis 分布式锁模块
+/// 提供基于 FreeRedis 的分布式锁服务配置
+/// </summary>
+public class TinyAbpFrameworkDistributedLockFreeRedisModule : AbpModule { }
