cabddd3a0744780ee4a56bf56be4f086b1e1153e
JessQiu <JessQiu@outlook.com>
2025-10-02 16:18:47 +0800

    feat(缓存): 添加FreeRedis缓存模块支持
    添加TinyAbp.Framework.Caching.FreeRedis项目，实现基于FreeRedis的分布式缓存功能
    在HttpApi.Host项目中添加对缓存模块的引用

---
+++ framework/TinyAbp.Framework.Caching.FreeRedis/TinyAbpFrameworkCachingFreeRedisModule.cs
@@ -0,0 +1,36 @@
+﻿using FreeRedis;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using Volo.Abp.Caching;
+using Volo.Abp.Modularity;
+
+namespace TinyAbp.Framework.Caching.FreeRedis;
+
+[DependsOn(typeof(AbpCachingModule))]
+public class TinyAbpFrameworkCachingFreeRedisModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        var configuration = context.Services.GetConfiguration();
+
+        var redisEnabled = configuration["Redis:IsEnabled"];
+        if (string.IsNullOrEmpty(redisEnabled) || bool.Parse(redisEnabled))
+        {
+            var redisConfiguration = configuration["Redis:Configuration"];
+            if (!redisConfiguration.IsNullOrEmpty())
+            {
+                var redisClient = new RedisClient(redisConfiguration);
+
+                context.Services.AddSingleton<IRedisClient>(redisClient);
+                context.Services.Replace(
+                    ServiceDescriptor.Singleton<IDistributedCache>(
+                        new DistributedCache(redisClient)
+                    )
+                );
+            }
+            ;
+        }
+    }
+}
